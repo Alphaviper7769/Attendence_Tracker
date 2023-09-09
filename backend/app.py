be02a1f2ad8dd43f8c69c36a56567912aeaea71c
Alphaviper7769 <aayushraturi@gmail.com>
2023-09-09 21:37:17 +0530

    Backend update

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -17,7 +18,7 @@ attendance_collection = db["Attendance"]
 def test_db():
     
     test_doc = {"message": "MongoDB connection successful!"}
-    collecteacher_collection.insert_one(test_doc)
+    id = teacher_collection.insert_one(test_doc).inserted_id
     return "Test document inserted into MongoDB."
 
 
@@ -26,14 +27,19 @@ def test_db():
 # REQUEST -> GET    /teacher/:id
 # RESPONSE ->       list of all the courses associated with the teacher id
 
+
+
 @app.route('/teacher/<teacher_id>', methods=['GET'])
 def get_teacher_courses(teacher_id):
+
     try:
         # Find the teacher's courses by teacher_id
-        teacher_courses = teacher_collection.find({"_id": teacher_id})
+        teacher_courses = teacher_collection.find_one({"_id": ObjectId(teacher_id)})
+        print(teacher_courses)
 
         # Convert the cursor to a list of dictionaries
         course_list = list(teacher_courses)
+        print(course_list)
 
         return jsonify(course_list), 200
     except Exception as e:
@@ -47,37 +53,20 @@ def get_teacher_courses(teacher_id):
 
 @app.route('/teacher/<teacher_id>/<course>/<date>', methods=['GET'])
 def get_teacher_course_attendance(teacher_id, course, date):
+    ans = []
     try:
         # Find the teacher's courses by teacher_id
-        teacher_courses = teacher_collection.find({"_id": teacher_id})
+        teacher_courses = teacher_collection.find({"_id": ObjectId(teacher_id)}).next()['course_id']
 
-        # Check if the requested course exists and is associated with the teacher
-        requested_course = None
-        for course_doc in teacher_courses:
-            if course_doc.get("course_name") == course:
-                requested_course = course_doc
-                break
-
-        if not requested_course:
-            return jsonify({"error": "Course not found or not associated with the teacher"}), 404
-
-        # Find students associated with the course
-        course_id = requested_course["_id"]
-        course_students = student_collection.find({"course_ids": course_id})
-
-        # Find attendance records for the requested course and date
-        attendance_records = attendance_collection.find({"course_id": course_id, "date": date})
-
-        student_attendance = {}
-
-        # Iterate through course students and populate the student_attendance dictionary
-        for student in course_students:
-            student_id = student["_id"]
-            student_name = student["name"]
-            attended = any(record["student_id"] == student_id and record["attended"] for record in attendance_records)
-            student_attendance[student_name] = "attended" if attended else "absent"
-
-        return jsonify(student_attendance), 200
+        all_students = course_collection.find({ "_id": ObjectId(course) }).next()['student_ids']
+        for student in all_students:
+            attended = attendance_collection.find({ "student_id": ObjectId(student), "date": date, "course_id": ObjectId(course) })
+            if not attended:
+                ans.append([student, "Absent"])
+            else:
+                ans.append([student, "Present"])
+        
+        return jsonify(ans), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -92,27 +81,19 @@ def get_teacher_course_attendance(teacher_id, course, date):
 def get_student_course_schedule_and_attendance(student_id, date):
     try:
         
-        student = student_collection.find_one({"_id": student_id})
+        student = student_collection.find_one({"_id": ObjectId(student_id)})
 
         if not student:
             return jsonify({"error": "Student not found"}), 404
 
         # Find the courses associated with the student
-        student_courses = course_collection.find({"student_ids": student_id})
-
-        # Find attendance records for the student on the specified date
-        attendance_records = attendance_collection.find({"student_id": student_id, "date": date})
-
-        
-        course_attendance = {}
-
-        
-        for course in student_courses:
-            course_id = course["_id"]
-            course_name = course["course_name"]
-            attended = any(record["course_id"] == course_id and record["attended"] for record in attendance_records)
-            course_attendance[course_name] = "attended" if attended else "absent"
-
+        courses = list(course_collection.find())
+        course_attendance = []
+        for course in courses:
+            if ObjectId(student_id) in course['student_ids']:
+                course_attendance.append([course['_id'], 'Present'])
+            else:
+                course_attendance.append([course['_id'], 'Absent'])
         return jsonify(course_attendance), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
